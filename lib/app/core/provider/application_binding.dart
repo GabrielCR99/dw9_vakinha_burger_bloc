3199a5b9cd3989a1525fe268097394fc6c3b3e40
Roveri <gabriel.roveri7@gmail.com>
2023-02-07 08:24:11 -0300

    refactor: added back provider for better semantic

--- lib/app/core/provider/application_binding.dart
+++ lib/app/core/provider/application_binding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 import '../../repositories/auth/auth_repository.dart';
 import '../../repositories/auth/auth_repository_impl.dart';
@@ -11,9 +12,9 @@ class ApplicationBinding extends StatelessWidget {
   const ApplicationBinding({required this.child, super.key});
 
   @override
-  Widget build(BuildContext context) => MultiRepositoryProvider(
+  Widget build(BuildContext context) => MultiProvider(
         providers: [
-          RepositoryProvider(create: (_) => CustomDio()),
+          Provider(create: (_) => CustomDio()),
           RepositoryProvider<AuthRepository>(
             create: (context) => AuthRepositoryImpl(dio: context.read()),
           ),
--- lib/app/models/auth_model.dart
+++ lib/app/models/auth_model.dart
@@ -2,10 +2,7 @@ class AuthModel {
   final String accessToken;
   final String refreshToken;
 
-  const AuthModel({
-    required this.accessToken,
-    required this.refreshToken,
-  });
+  const AuthModel({required this.accessToken, required this.refreshToken});
 
   factory AuthModel.fromMap(Map<String, dynamic> map) => AuthModel(
         accessToken: map['access_token'] ?? '',
